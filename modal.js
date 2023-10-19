a13929e1a4642682592e5de60dd32746e8f7cbb8
Milla22 <marti.ludmilla@orange.fr>
2023-10-19 17:01:59 +0200

    creation et gestion des messages d'erreur

--- modal.js
+++ modal.js
@@ -14,10 +14,10 @@ const formData = document.querySelectorAll(".formData");
 const closeX = document.querySelector(".close");
 
 // Réinitialisation du formulaire après fermeture de la modale
-function resetForm() {
+function resetForm(){
   document.querySelector('form[name="reserve"]').style.display = "block";
   document.getElementById("message").style.display = "none";
-  // Réinitialise les champs du formulaire après fermeture de la modale
+  // Réinitialisation des champs du formulaire après fermeture de la modale
   document.getElementById("first").value = "";
   document.getElementById("last").value = "";
   document.getElementById("email").value = "";
@@ -26,6 +26,19 @@ function resetForm() {
   const locations = document.querySelectorAll('input[name="location"]');
   locations.forEach(location => location.checked = false);
   document.getElementById("checkbox1").checked = false;
+  resetErrors();
+
+  // Réinitialisation du message de confirmation
+  const confirmationMessage = document.querySelector('.validation-message');
+  if (confirmationMessage) {
+    confirmationMessage.remove();
+  }
+
+}
+
+function resetErrors() {
+  const errorMessages = document.querySelectorAll('.error-msg');
+  errorMessages.forEach(message => message.style.display = 'none');
 }
 
 // launch modal event
@@ -42,12 +55,14 @@ function closeModal() {
   closeX.addEventListener('click', function(e) {
     e.preventDefault();
     modalbg.style.display="none";
+    resetForm();
   });
 }
 closeModal();
 
 //Validation du formulaire
 function validateForm(event) {
+  resetErrors();
   // Empêche la soumission du formulaire pour éviter la fermeture de la modale
   event.preventDefault(); 
   // Les éléments du DOM
@@ -58,40 +73,112 @@ function validateForm(event) {
   const quantity = document.getElementById("quantity").value;
   const location = document.querySelector('input[name="location"]:checked');
   const checkbox1 = document.getElementById("checkbox1");
+
+// Creation des messages d'erreurs
+  const errorFirst = document.createElement('p');
+  errorFirst.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+  errorFirst.classList.add('error-msg');
+  const messageErrorFirst = document.getElementById('error-first');
+  messageErrorFirst.appendChild(errorFirst);
+  console.log(errorFirst)
+
+  const errorLast = document.createElement('p');
+  errorLast.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+  errorLast.classList.add('error-msg');
+  const messageErrorLast = document.getElementById('error-last');
+  messageErrorLast.appendChild(errorLast);
+
+  const errorEmail = document.createElement('p');
+  errorEmail.textContent = "Veuillez entrer une adresse e-mail valide.";
+  errorEmail.classList.add('error-msg');
+  const messageErrorEmail = document.getElementById('error-email');
+  messageErrorEmail.appendChild(errorEmail);
+
+  const errorBirthdate = document.createElement('p');
+  errorBirthdate.textContent = "Vous devez entrer votre date de naissance";
+  errorBirthdate.classList.add('error-msg');
+  const messageErrorBirthdate = document.getElementById('error-birthdate');
+  messageErrorBirthdate.appendChild(errorBirthdate);
+
+  const errorQuantity = document.createElement('p');
+  errorQuantity.textContent = "Vous devez entrer un nombre entre 0 et 99.";
+  errorQuantity.classList.add('error-msg');
+  const messageErrorQuantity = document.getElementById('error-quantity');
+  messageErrorQuantity.appendChild(errorQuantity);
+
+  const errorLocation = document.createElement('p');
+  errorLocation.textContent = "Vous devez choisir une localité";
+  errorLocation.classList.add('error-msg');
+  const messageErrorLocation = document.getElementById('error-location');
+  messageErrorLocation.appendChild(errorLocation);
+
+  const errorCU = document.createElement('p');
+  errorCU.textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
+  errorCU.classList.add('error-msg');
+  const messageErrorCU = document.getElementById('error-CU');
+  messageErrorCU.appendChild(errorCU);
+
   // Validation = true
-  const valid = true;
+  let valid = true;
   //Si le prénom a moins de 2 caractères, formulaire non valide
   if (firstName.length < 2 || firstName.trim() === "") {
+      messageErrorFirst.style.display="flex";
       valid = false;
+  } else {
+    messageErrorFirst.style.display="none";
   }
   //Si le nom a moins de 2 caractères, formulaire non valide
   if (lastName.length < 2 || lastName.trim() === "") {
+      messageErrorLast.style.display="flex";
       valid = false;
+  } else {
+    messageErrorLast.style.display="none";
   }
   //Si l'adresse électronique n'est pas valide (function isValidEmail), formulaire non valide
   if (!isValidEmail(email)) {
+      messageErrorEmail.style.display="flex";
       valid = false;
+  } else {
+    messageErrorEmail.style.display="none";
   }
   // Si la date de naissance n'est pas renseignée, formulaire non valide
   if (birthDate.trim()=== "") {
+      messageErrorBirthdate.style.display="flex";
       valid = false;
+  } else {
+    messageErrorBirthdate.style.display="none";
   }
   // Si vide ou nombre < 0 ou nombre > 99, formulaire non valide
-  if (isNaN(quantity) || quantity < 0 || quantity > 99) {
+  if (quantity === "" || isNaN(quantity) || quantity < 0 || quantity > 99) {
+      messageErrorQuantity.style.display="flex";
       valid = false;
+  } else {
+    messageErrorQuantity.style.display="none";
   }
   // Si une localité n'est pas checkée, formulaire non valide
   if (!location) {
+      messageErrorLocation.style.display="flex";
       valid = false;
+  } else {
+    messageErrorLocation.style.display="none";
   }
   // Si la coche CU n'est pas checkée,  formulaire non valide
   if (!checkbox1.checked) {
+      messageErrorCU.style.display="flex";
       valid = false;
+  } else {
+    messageErrorCU.style.display="none";
   }
   if (valid) {
+    // Création du message de conformation d'inscription dans la div id message
+    const validationMessage = document.createElement('div');
+    validationMessage.textContent = "Merci! Votre réservation a bien été enregistrée.";
+    validationMessage.classList.add('validation-message');
+
     // Si le formulaire est valide, masquage du formulaire et affichage du message de validation.
     document.querySelector('form[name="reserve"]').style.display = "none";
-    document.getElementById("message").style.display = "flex";
+    const modalBody = document.querySelector('.modal-body');
+    modalBody.appendChild(validationMessage);
   }
 }
 //Vérification si chaîne de caractères email correspond à un format valide avec une expression régulière regex
@@ -101,6 +188,8 @@ function isValidEmail(email) {
   //méthode test() renvoie true si la correspondance est trouvée
   return emailRegex.test(email);
 }
+
+
 
 
 
